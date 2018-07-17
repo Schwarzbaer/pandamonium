804e29644040402a99ab2d1e9ba201419cbfe488
Sebastian Hoffmann <TheCheapestPixels@gmail.com>
2018-07-18 01:19:00 +0200

    IDGenerator is now threadsafe.

--- pandamonium/util.py
+++ pandamonium/util.py
@@ -1,8 +1,12 @@
-# TODO: Make threadsafe
+from threading import Lock
+
+
 class IDGenerator:
     def __init__(self, start_id=0):
         self.counter = start_id - 1
+        self.lock = Lock()
 
     def get_new(self):
-        self.counter += 1
-        return self.counter
+        with self.lock:
+            self.counter += 1
+            return self.counter
